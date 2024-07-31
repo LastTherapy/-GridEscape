a39d66df905be7825f7a50c398c3301e4695f986
Alice Zvyagina <alicezvyagina@MacBook-Air-Alice.local>
2024-07-31 14:37:09 +0300

    enemy

--- src/main/java/ru/dobrocraft/game/Enemy.java
+++ src/main/java/ru/dobrocraft/game/Enemy.java
@@ -1,4 +1,44 @@
 package ru.dobrocraft.game;
 
+import ru.dobrocraft.game.GameMap;
+import ru.dobrocraft.game.Movable;
+import ru.dobrocraft.game.Direction;
+
+import lombok.Getter;
 public class Enemy {
+    private int x;
+    private int y;
+
+    public Enemy(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move(Direction direction, GameMap gameMap) {
+        int newX = getX();
+        int newY = getY();
+        switch (direction) {
+            case LEFT:
+                newX--;
+                break;
+            case UP:
+                newY--;
+                break;
+            case RIGHT:
+                newX++;
+                break;
+            case DOWN:
+                newY++;
+                break;
+        }
+        if (gameMap.getData[newX][newY] == GameObject.EMPTY.getValue()) {
+            if (newX < 0 || newX >= gameMap.getSize() || newY < 0 || newY >= gameMap.getSize()) {
+                return;
+            }
+            gameMap.getData[getX()][getY()] = GameObject.EMPTY.getValue();
+            gameMap.getData[newX][newY] = GameObject.ENEMY.getValue();
+            x = newX;
+            y = newY;
+        }
+    }
 }
