9ca5e8be39ee3cef8df912f9c50073347b8b91e4
helllomind <helllomind@hellomindmac.local>
2024-07-31 23:16:34 +0300

    background_color

--- src/main/java/ru/dobrocraft/game/Printer.java
+++ src/main/java/ru/dobrocraft/game/Printer.java
@@ -1,7 +1,12 @@
 package ru.dobrocraft.game;
+import com.diogonunes.jcdp.color.ColoredPrinter;
+import com.diogonunes.jcdp.color.api.Ansi;
+import com.diogonunes.jcdp.color.api.Ansi.BColor;
+import com.diogonunes.jcdp.color.api.Ansi.FColor;
 
 public class Printer {
     PrinterSettings settings = new PrinterSettings();
+    FColor fColor = FColor.BLACK;
 
 
     public void printMap(GameMap gameMap) {
@@ -10,11 +15,35 @@ public class Printer {
             System.out.print('|');
             for (int j = 0; j < gameMap.getSize(); j++) {
                 int  gameObject = gameMap.getData()[j][i];
-                System.out.print(charObject(gameObject));
+                char charObject = charObject(gameObject);
+                BColor bColor = getGameObjetColor(gameObject);
+                ColoredPrinter coloredPrinter = new ColoredPrinter.Builder(1, false)
+                        .foreground(fColor)
+                        .background(bColor)
+                        .attribute(Ansi.Attribute.BOLD)
+                        .build();
+                coloredPrinter.print(charObject);
+
             }
             System.out.println('|');
         }
         System.out.println("_".repeat(gameMap.getSize() + 2));
+    }
+
+
+    private BColor getGameObjetColor(int gameObject) {
+        switch (gameObject) {
+            case 0:
+                return BackGroundColor.getBColor(settings.getPlayerColor());
+            case 1:
+                return BackGroundColor.getBColor(settings.getEnemyColor());
+            case 2:
+                return BackGroundColor.getBColor(settings.getWallColor());
+            case 3:
+                return BackGroundColor.getBColor(settings.getGoalColor());
+            default:
+                return BackGroundColor.getBColor(settings.getEmptyColor());
+        }
     }
 
     private char charObject(int gameObject) {
@@ -35,5 +64,12 @@ public class Printer {
             default:
                 return settings.getEmptyChar();
         }
+    }
+}
+
+class BackGroundColor {
+    public static BColor getBColor(String color) {
+        Ansi.BColor bColor = Ansi.BColor.valueOf(color);
+        return bColor;
     }
 }
