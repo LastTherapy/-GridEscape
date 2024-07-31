c603f88db7f82f97b3342712088752ab740b8f22
Alice Zvyagina <alicezvyagina@MacBook-Air-Alice.local>
2024-07-31 13:58:35 +0300

    movable

--- src/main/java/ru/dobrocraft/game/Movable.java
+++ src/main/java/ru/dobrocraft/game/Movable.java
@@ -1,5 +1,7 @@
 package ru.dobrocraft.game;
 
 public interface Movable {
-    public int move(Direction direction);
+    public int move(Direction direction, GameMap gameMap);
+    int getX();
+    int getY();
 }
