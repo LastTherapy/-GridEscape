69fe30522b6dd1854344592fd70102cdffd7cde1
Alice Zvyagina <alicezvyagina@MacBook-Air-Alice.local>
2024-08-03 17:35:00 +0300

    ChaseLogic

--- src/chaselogic/src/main/java/ru/dobrocraft/Node.java
+++ src/chaselogic/src/main/java/ru/dobrocraft/Node.java
@@ -3,24 +3,19 @@ package main.java.ru.dobrocraft;
 import lombok.Getter;
 @Getter
 public class Node {
-    private Position position;
+    private int x;
+    private int y;
     private int g; // стоимость пути от старта
     private int h; // стоимость пути до цели
     private int f; // g + h
     private Node parent;
 
-    public Node(Position position, Node parent, int g, int h) {
-        this.position = position;
+    public Node(int x, int y, Node parent, int g, int h) {
+        this.x = x;
+        this.y = y;
         this.g = g;
         this.h = h;
         this.f = g + h;
         this.parent = parent;
-    }
-
-    class NodeComparator implements java.util.Comparator<Node> {
-        @Override
-        public int compare(Node n1, Node n2) {
-            return Integer.compare(n1.getF(), n2.getF());
-        }
     }
 }
